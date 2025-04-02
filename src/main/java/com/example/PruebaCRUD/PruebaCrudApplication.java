d23f0dc11dc47e01801fa631982895f3d3fec804
Alfredo Jiménez <jajr0316@gmail.com>
2025-04-02 13:07:08 -0600

    Configuraciones

--- src/main/java/com/example/PruebaCRUD/PruebaCrudApplication.java
+++ src/main/java/com/example/PruebaCRUD/PruebaCrudApplication.java
@@ -2,9 +2,15 @@ package com.example.PruebaCRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.PruebaCRUD.Repositories")
+@ComponentScan(basePackages = "com.example.PruebaCRUD")
+@EntityScan(basePackages = "com.example.PruebaCRUD.BD")
 @RestController // Notación que hace que esto se vuelva un controlador.
 public class PruebaCrudApplication {
 
