ec04bc2828e140fc478a87f7e011e9f05258e669
Alfredo Jiménez <jajr0316@gmail.com>
2025-04-17 01:59:27 -0600

    a

--- src/main/java/com/example/PruebaCRUD/PruebaCrudApplication.java
+++ src/main/java/com/example/PruebaCRUD/PruebaCrudApplication.java
@@ -2,22 +2,16 @@ package com.example.PruebaCRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RestController;
-import jakarta.annotation.PostConstruct;
-
-import java.io.File;
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.PruebaCRUD.Repositories")
 @ComponentScan(basePackages = "com.example.PruebaCRUD")
 @EntityScan(basePackages = "com.example.PruebaCRUD.BD")
 @RestController // Notación que hace que esto se vuelva un controlador.
-//@EnableScheduling
 public class PruebaCrudApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/PruebaCRUD/utils/CloudinaryUploader.java
+++ src/main/java/com/example/PruebaCRUD/utils/CloudinaryUploader.java
@@ -2,15 +2,19 @@ package com.example.PruebaCRUD.utils;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Map;
 
+@Component
 public class CloudinaryUploader {
 
     private final Cloudinary cloudinary;
 
+    @Autowired
     public CloudinaryUploader(Cloudinary cloudinary) {
         this.cloudinary = cloudinary;
     }
