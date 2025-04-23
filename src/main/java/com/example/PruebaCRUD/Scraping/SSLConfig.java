714a3e581ce3b0daeb301c0d3f7160b2e3f31252
Alfredo Jiménez <jajr0316@gmail.com>
2025-04-23 12:32:16 -0600

    SSL

--- src/main/java/com/example/PruebaCRUD/Scraping/SSLConfig.java
+++ src/main/java/com/example/PruebaCRUD/Scraping/SSLConfig.java
@@ -6,58 +6,33 @@ import java.security.KeyStore;
 import java.security.cert.CertificateFactory;
 
 public class SSLConfig {
-    private static boolean isConfigured = false;
-
-    public static void configureSSL() throws Exception {
-        if (isConfigured) {
-            return;
-        }
-
+    public static void configureSSL() {
         try {
-            InputStream certStream = SSLConfig.class.getResourceAsStream("/certs/ipn.mx.crt");
-            if (certStream == null) {
-                // Si no encuentra el certificado, usa configuración alternativa
-                configurarSSLAlternativo();
-                return;
-            }
-
+            // Usar directamente la ruta del archivo, que sabemos que funciona
+            FileInputStream fis = new FileInputStream("src/main/resources/certs/ipn.mx.crt");
             KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
             keyStore.load(null, null);
-
             CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            keyStore.setCertificateEntry("ipn_cert", cf.generateCertificate(certStream));
+            keyStore.setCertificateEntry("ipn_cert", cf.generateCertificate(fis));
 
+            // Crear TrustManager que use el certificado
             TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             tmf.init(keyStore);
 
+            // Configurar SSLContext
             SSLContext sc = SSLContext.getInstance("TLS");
             sc.init(null, tmf.getTrustManagers(), null);
 
+            // Establecer como contexto SSL predeterminado
             HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> hostname.endsWith(".ipn.mx"));
 
-            isConfigured = true;
+            fis.close();
+            System.out.println("Certificado cargado exitosamente");
 
         } catch (Exception e) {
-            configurarSSLAlternativo();
+            System.err.println("Error al cargar el certificado: " + e.getMessage());
+            e.printStackTrace();
+            // No usar configuración de respaldo, para que sepamos si hay un problema
         }
-    }
-
-    private static void configurarSSLAlternativo() throws Exception {
-        SSLContext sc = SSLContext.getInstance("TLS");
-        sc.init(null, new TrustManager[]{new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-            public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-            }
-            public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-            }
-        }}, new java.security.SecureRandom());
-
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
-
-        isConfigured = true;
     }
 }
